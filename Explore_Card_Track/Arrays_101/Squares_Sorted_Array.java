f65255a66f1e3f85b0170dc5bd91f4c800c74fd3
Ryan Tusi <ryantusi45@gmail.com>
2025-01-12 15:25:27 +0530

    SSA2

--- Explore_Card_Track/Arrays_101/Squares_Sorted_Array.java
+++ Explore_Card_Track/Arrays_101/Squares_Sorted_Array.java
@@ -11,12 +11,38 @@ Explanation: After squaring, the array becomes [16,1,0,9,100].
 After sorting, it becomes [0,1,9,16,100].
  */
 
-class Solution {
+/*class Solution {
     public int[] sortedSquares(int[] nums) {
         for(int i = 0; i < nums.length; i++) {
             nums[i] = nums[i]*nums[i];
         }
         Arrays.sort(nums);
         return nums;
+    }
+}*/
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1, index = n -1;
+        int left2 = 0, right2 = 0;
+
+        while(left <= right) {
+            left2 = nums[left] * nums[left];
+            right2  = nums[right] * nums[right];
+
+            if(left2 > right2) {
+                result[index] = left2;
+                left++;
+            } else {
+                result[index] = right2;
+                right--;
+            }
+
+            index--;
+        }
+
+        return result;
     }
 }
