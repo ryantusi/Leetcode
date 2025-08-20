31b09a69986f809d9897a7576e541e5d4e39ec86
Ryan Tusi <ryantusi45@gmail.com>
2025-08-20 16:16:45 +0530

    Add Two Numbers

---
+++ Explore_Card_Track/Linked_List/AddTwoNumber.java
@@ -0,0 +1,50 @@
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+// Example 1:
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
+
+public class AddTwoNumber {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        int carry = 0;
+        int x = 0;
+        int y = 0;
+        int sum = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            x = (l1 != null) ? l1.val : 0;
+            y = (l2 != null) ? l2.val : 0;
+            sum = x + y + carry;
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
