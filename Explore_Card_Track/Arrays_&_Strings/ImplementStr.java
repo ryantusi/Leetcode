b2f21a3e8e7f3b810f8240fb0f72f36aa6e02f4c
Ryan Tusi <ryantusi45@gmail.com>
2025-09-03 15:40:48 +0530

    Implement strStr

---
+++ Explore_Card_Track/Arrays_&_Strings/ImplementStr.java
@@ -0,0 +1,31 @@
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+public class ImplementStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0)
+            return 0; // edge case
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i; // found first occurrence
+            }
+        }
+        return -1; // not found
+    }
+}
