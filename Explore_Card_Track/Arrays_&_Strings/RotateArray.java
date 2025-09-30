78773aef8fbe05cd4166d8cd83d90fb1e54fe653
Ryan Tusi <ryantusi45@gmail.com>
2025-09-30 19:43:04 +0530

    Rotate Array

---
+++ Explore_Card_Track/Arrays_&_Strings/RotateArray.java
@@ -0,0 +1,42 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // handle k > n
+
+        reverse(nums, 0, n - 1); // Step 1: reverse whole array
+        reverse(nums, 0, k - 1); // Step 2: reverse first k elements
+        reverse(nums, k, n - 1); // Step 3: reverse rest
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
