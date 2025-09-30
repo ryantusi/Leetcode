2308877f3f465bd0b739814ceb4e6e630294a1fe
Ryan Tusi <ryantusi45@gmail.com>
2025-09-30 20:26:14 +0530

    Pascals Triangle 2

---
+++ Explore_Card_Track/Arrays_&_Strings/PascalsTriangle2.java
@@ -0,0 +1,44 @@
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+ 
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle2 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        
+        for(int i = 0; i <= rowIndex; i++) {
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            
+            for(int j = 1; j < i; j++) {
+                int val = row.get(j-1) + row.get(j);
+                newRow.add(val);
+            }
+            
+            if(i > 0) newRow.add(1);
+            row = newRow;
+        }
+        
+        return row;
+    }
+}
