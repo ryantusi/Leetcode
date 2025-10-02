fd35afc675eba376e22ffe1429de93584ac678d3
Ryan Tusi <ryantusi45@gmail.com>
2025-10-02 22:24:16 +0530

    Hash Set

---
+++ Explore_Card_Track/HashTables/HashSet.java
@@ -0,0 +1,71 @@
+// Design a HashSet without using any built-in hash table libraries.
+
+// Implement MyHashSet class:
+
+//     void add(key) Inserts the value key into the HashSet.
+//     bool contains(key) Returns whether the value key exists in the HashSet or not.
+//     void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+ 
+
+// Example 1:
+
+// Input
+// ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+// [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+// Output
+// [null, null, null, true, false, null, true, null, false]
+
+// Explanation
+// MyHashSet myHashSet = new MyHashSet();
+// myHashSet.add(1);      // set = [1]
+// myHashSet.add(2);      // set = [1, 2]
+// myHashSet.contains(1); // return True
+// myHashSet.contains(3); // return False, (not found)
+// myHashSet.add(2);      // set = [1, 2]
+// myHashSet.contains(2); // return True
+// myHashSet.remove(2);   // set = [1]
+// myHashSet.contains(2); // return False, (already removed)
+
+import java.util.*;
+
+class HashSet {
+    private final int SIZE = 100; // class field (not inside constructor)
+    private LinkedList<Integer>[] buckets; // also class field
+
+    public HashSet() {
+        buckets = new LinkedList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(int key) { // must return int
+        return key % SIZE;
+    }
+
+    public void add(int key) {
+        int i = hash(key);
+        if (!buckets[i].contains(key)) {
+            buckets[i].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int i = hash(key);
+        buckets[i].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int i = hash(key);
+        return buckets[i].contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new HashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
