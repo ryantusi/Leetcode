255fed6623ba512efcbacb25346f6fe2401d1f51
Ryan Tusi <ryantusi45@gmail.com>
2025-10-04 21:02:53 +0530

    Minimum Index Sum

---
+++ Explore_Card_Track/HashTables/MinimumIndexSum.java
@@ -0,0 +1,66 @@
+// Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+
+// A common string is a string that appeared in both list1 and list2.
+
+// A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
+
+// Return all the common strings with the least index sum. Return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+// Output: ["Shogun"]
+// Explanation: The only common string is "Shogun".
+
+// Example 2:
+
+// Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+// Output: ["Shogun"]
+// Explanation: The common string with the least index sum is "Shogun" with index sum = (0 + 1) = 1.
+
+// Example 3:
+
+// Input: list1 = ["happy","sad","good"], list2 = ["sad","happy","good"]
+// Output: ["sad","happy"]
+// Explanation: There are three common strings:
+// "happy" with index sum = (0 + 1) = 1.
+// "sad" with index sum = (1 + 0) = 1.
+// "good" with index sum = (2 + 2) = 4.
+// The strings with the least index sum are "sad" and "happy".
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MinimumIndexSum {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        List<String> result = new ArrayList<>();
+        int minSum = Integer.MAX_VALUE;
+
+        for (int j = 0; j < list2.length; j++) {
+            String restaurant = list2[j];
+            if (map.containsKey(restaurant)) {
+                int sum = j + map.get(restaurant);
+                
+                if (sum < minSum) {
+                    // Found smaller index sum → new result list
+                    minSum = sum;
+                    result.clear();
+                    result.add(restaurant);
+                } else if (sum == minSum) {
+                    // Same minimum sum → add to result
+                    result.add(restaurant);
+                }
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
